d2957b1ab1faaedd0ccd5db0896826a51230b438
“linhPham98” <linhthusinh98@gmail>
2024-11-23 19:04:41 +0900

    commit adding api update

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const supabase = createClient(supabaseUrl, supabaseKey);
 
 const app = express();
 app.use(express.json());
-const port = 5000;
+const port = 3500;
 
 const corsOptions = {
   origin: ["https://my-private.vercel.app", "http://localhost:5173"],
@@ -23,7 +23,8 @@ app.get("/api/list", async (req, res) => {
   try {
     const { data, error } = await supabase
       .from("privates")
-      .select("*");
+      .select("*")
+      .order('id', { ascending: true });
 
     if (error) {
       console.error("Error fetching data:", error);
@@ -37,9 +38,34 @@ app.get("/api/list", async (req, res) => {
   }
 });
 
+app.patch('/api/update/:id', async (req, res) => {
+  const { id } = req.params; // Lấy id từ URLs
+  const { status, time } = req.body; // Lấy dữ liệu cập nhật từ body
+  const converIdToNumber = Number.parseInt(id); // Chuyển id sang kiểu số
+
+  // Chuyển timeFromBody thành kiểu timestamp hợp lệ
+  const validTime = time ? new Date(time).toISOString() : null;
+
+  try {
+    // Cập nhật bản ghi trong bảng `privates` bằng Supabase
+    const { data, error } = await supabase
+      .from("privates")
+      .update({ time: validTime, status: status })
+      .eq("id", converIdToNumber)
+      .select();
+    if (error) {
+      console.error("Error updating data:", error);
+      return res.status(500).json({ error: "Failed to update data" });
+    }
+    return res.status(200).json(data[0]);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: 'Internal server error', error });
+  }
+});
+
 app.listen(port, () => {
   console.log(`API is running on http://localhost:${port}`);
 });
 
-// Xuất handler cho Vercel
 export default app;
